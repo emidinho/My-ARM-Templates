335e943c3ff9696412e2cfc13243aabad8e45707
emidinho <emidioalemnju@yahoo.com>
2023-10-03 17:28:27 +0800

    update

---
+++ AzurePSdeplymentscript.ps1
@@ -0,0 +1,8 @@
+// $ResourceGroupName="RG1"
+$templateFile="azuredeploy.json"
+$today=Get-Date -Format "MM-dd-yyyy"
+$deploymentName="testtemplate-"+"$today"
+New-AzResourceGroupDeployment `
+  -Name $deploymentName `
+  -TemplateFile $templateFile `
+  -ResourceGroupName $ResourceGroupName
